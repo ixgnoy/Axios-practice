2beeb41310de8328ce28b168879acacc137abaf6
Wong Yong Xi <115607618+ixgnoy@users.noreply.github.com>
2025-05-11 18:39:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,34 +15,37 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
-    res.render("index.ejs", { data: result, error: null });
+    console.log(result);
+    res.render("index.ejs", { data: result });
   } catch (error) {
-    res.render("index.ejs", { data: null, error: error.message });
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
   }
 });
 
-
 app.post("/", async (req, res) => {
-  try {
-    const type = req.body.type;
+  try{
+    console.log(req.body);
+    const type=req.body.type;
     const participants = req.body.participants;
-
     const response = await axios.get(
-      `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`
+      https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}
     );
-
-    const result = response.data;
-    res.render("index.ejs", { data: result, error: null });
-
-  } catch (error) {
+    const result=response.data;
+    console.log(result);
     res.render("index.ejs", {
-      data: null,
+      data: result[Math.floor(Math.random()*result.length)],
+    });
+  }catch(error){
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
       error: "No activities that match your criteria.",
     });
   }
 });
 
-
 app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
+  console.log(Server running on port: ${port});
 });
